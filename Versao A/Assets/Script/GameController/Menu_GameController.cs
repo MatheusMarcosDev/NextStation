55a674fb94325ae41fb097f62455f70e4f6dd950
MatheusMarcos <88052109+MatheusMarcosDev@users.noreply.github.com>
2022-01-12 10:16:05 -0300

    12/01

---
+++ Versao A/Assets/Script/GameController/Menu_GameController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_GameController : MonoBehaviour
+{
+    public GameObject panelCredits;
+    [SerializeField] private bool isPanelCredits;
+
+    private void Start()
+    {
+        panelCredits.SetActive(false);
+
+        isPanelCredits = false;
+    }
+
+    public void Play()
+    {
+
+    }
+
+    public void Tutorial()
+    {
+
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void Credits()
+    {
+        if(!isPanelCredits)
+        {
+            panelCredits.SetActive(true);
+            isPanelCredits = !isPanelCredits;
+            Debug.Log("Abrir");
+        }
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                panelCredits.SetActive(false);
+                isPanelCredits = !isPanelCredits;
+                Debug.Log("Fechar");
+            }
+        }
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
